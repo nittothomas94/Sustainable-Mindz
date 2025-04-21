317d080a8a017df9fcf649b9053a9b834eba5790
nittothomas <nittothomas94@example.com>
2025-04-21 16:57:11 +0530

    updated footer

--- src/app/components/footer/page.js
+++ src/app/components/footer/page.js
@@ -1,21 +1,16 @@
 export default function Footer() {
   return (
-    <div className="bg-gradient-to-b from-purple-300 to-purple-100 h-15">
-      <p className="w-fit md:w-[400px] size-[15px]">
+    <div className="bg-gradient-to-b from-purple-300 to-purple-100 h-15 flex items-center justify-between md:px-[110px]">
+      <p className="w-fit  size-[15px]">
         Copyright © 2024 Sustainable Mindz. All Rights Reserved.
       </p>
 
-      <div className="flex gap-2">
+      <div className="flex gap-2 items-center justify-center">
         <i className="material-icons text-white bg-purple-900 rounded-2xl p-[5px]">
           facebook
         </i>
-
-        <i className="material-icons text-white bg-purple-900 rounded-2xl p-[5px]">
-          instagram
-        </i>
-        <i className="material-icons text-white bg-purple-900 rounded-2xl p-[5px]">
-          linkedin
-        </i>
+        <i class="fa-brands fa-instagram"></i>
+        <i class="fa-brands fa-linkedin"></i>
       </div>
     </div>
   );
